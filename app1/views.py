9a58a6b99b0b13040e1c3f6661ed1a820eef15c5
Victus <otabekrahmiddinov08@gmail.com>
2025-09-26 14:40:14 +0500

    .

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,19 @@
+
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from app1.models import Programms
+
 
 # Create your views here.
+class ProgramAddView(CreateView):
+    model = Programms
+    template_name = 'programm_add.html'
+    fields = ['name','summary','document']
+
+    def get_success_url(self):
+        return reverse_lazy('programm-list')
+
+class AntivirusListView(ListView):
+    model = Programms
+    template_name = 'antivirus_list.html'
