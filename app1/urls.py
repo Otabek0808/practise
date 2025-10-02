527192a31c76241b0c3947be3b8d3e504473a359
Victus <otabekrahmiddinov08@gmail.com>
2025-10-02 14:13:16 +0500

    .

--- app1/urls.py
+++ app1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from app1.views import AntivirusListView, OSListView, MobileListView, CompListView
+from app1.views import AntivirusListView, OSListView, MobileListView, CompListView, AntivirusDetailView
 
 urlpatterns = [
     path('antivirus/', AntivirusListView.as_view(), name='antivirus-list'),
     path('os/', OSListView.as_view(), name='os-list'),
     path('kompyuter-dastur', CompListView.as_view(), name='comp-list'),
     path('mobile-dastur', MobileListView.as_view(), name='mobile-list'),
+    path('<int:pk>/', AntivirusDetailView.as_view(), name='antivirus-detail'),
 ]
